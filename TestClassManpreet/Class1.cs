a48732b24f839cafec4f915a7b3b9ce6ab6ea8e7
Manpreet Kaur <mkaur5967@conestogac.on.ca>
2024-06-06 03:13:25 -0400

    Add project files.

---
+++ TestClassManpreet/Class1.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using NunitDemo;
+namespace TestClassManpreet
+
+{
+    [TestFixture]
+    public class TriangleTest1
+    {
+        // Manpreet's Test
+        [Test]
+        public void ValidTriangle_Input60and60and60_OutputValidTriangle()
+        {
+            // Arrange
+       
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 60;
+            string expected = "The triangle is valid.";
+            // Act
+            string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
